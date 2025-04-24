a112205f1ca9e82f60bcd55a68b8656dfdcdbf4f
ラマ　ビサル <lb1627565@gmail.com>
2025-04-24 12:31:26 +0900

    recoloring

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,24 +3,24 @@ import React from 'react';
 const Header = ({ setCategory }) => {
   return (
     <div className='App text-center m-3' style={{ cursor: 'default' }}>
-      <h1 className=' border bg-white rounded-full p-2 font-bold'>News Feed</h1>
-      <div className="category-selector border my-2 font-bold bg-[#EEE3CB] p-2">
-        <button className='border rounded-full
+      <h1 className=' bg-white p-2 font-bold text-2xl'>News Feed</h1>
+      <div className="category-selector my-2 font-bold bg-[#FFFFFF] p-2">
+        <button className='border border-blue-600 rounded-full
          transition-transform duration-200 
          hover:-translate-y-1 hover:scale-105' 
          style={{ cursor: 'default' }} 
          onClick={() => setCategory('general')}>General</button>
-        <button className='border rounded-full
+        <button className='border border-blue-600 rounded-full
         transition-transform duration-200 
         hover:-translate-y-1 hover:scale-105' 
         style={{ cursor: 'default' }} 
         onClick={() => setCategory('business')}>Business</button>
-        <button className='border rounded-full
+        <button className='border border-blue-600 rounded-full
         transition-transform duration-200 
         hover:-translate-y-1 hover:scale-105' 
         style={{ cursor: 'default' }} 
         onClick={() => setCategory('technology')}>Technology</button>
-        <button className='border rounded-full
+        <button className='border border-blue-600 rounded-full
         transition-transform duration-200 
         hover:-translate-y-1 hover:scale-105' 
         style={{ cursor: 'default' }} 
